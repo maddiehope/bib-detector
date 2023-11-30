5a56007cf91702d26ecab8a8e9d46fbb9163cdfa
maddiehope <112491662+maddiehope@users.noreply.github.com>
2023-11-30 02:09:36 -0500

    bare bones flask app

---
+++ app.py
@@ -0,0 +1,31 @@
+'''
+    BIB DETECTION FLASK APP
+'''
+
+# IMPORTS: -------------------------------------------------------------------------------------------------------------------
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import requests
+import pandas as pd
+from flask_cors import CORS
+import sqlite3
+import os
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
+# FLASK CONFIGURATION/SETUP: -------------------------------------------------------------------------------------------------
+
+# creating the Flask 
+app = Flask(__name__, template_folder='templates')
+CORS(app)
+
+# "Home" page, route "/"
+@app.route('/', methods=['GET'])
+def selection():
+    return render_template("home.html",title="Home") 
+
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
+app.run(debug=True, port=8080) # can be commented out when deployed
